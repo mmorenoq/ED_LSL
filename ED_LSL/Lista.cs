290263809cf4a29d2f1da633930a252773bf8be0
Miguel Moreno <mmorenoher@gmail.com>
2020-05-02 21:58:41 -0700

    Actividad completada con funciones de agregar nodos y de busqueda

--- ED_LSL/Lista.cs
+++ ED_LSL/Lista.cs
@@ -58,5 +58,92 @@ namespace ED_LSL
             nodoNuevo.Siguiente = null;
             nodoActual.Siguiente = nodoNuevo;
         }
+        public void AgregarNodoInicio(string Dato)
+        {
+            Nodo NodoNuevo = new Nodo();
+            NodoNuevo.Dato = Dato;
+            NodoNuevo.Siguiente = primerNodo.Siguiente;
+            primerNodo.Siguiente = NodoNuevo;
+        }
+        public Nodo Buscar(string Dato)
+        {
+            if (EstaVacia())
+	        {
+                return null;
+	        }
+
+            Nodo NodoBusqueda = new Nodo();
+            NodoBusqueda = primerNodo;
+
+            while (NodoBusqueda.Siguiente != null)
+            {
+                NodoBusqueda = NodoBusqueda.Siguiente;
+                if (NodoBusqueda.Dato == Dato)
+                {
+                    return NodoBusqueda;
+                }
+            }
+
+            return null;
+        }
+        public Nodo BuscarAnterior(string Dato)
+        {
+            if (EstaVacia())
+            {
+                return null;
+            }
+
+            Nodo NodoBusqueda = new Nodo();
+            NodoBusqueda = primerNodo;
+
+            while (NodoBusqueda.Siguiente != null && NodoBusqueda.Siguiente.Dato != Dato)
+            {
+                NodoBusqueda = NodoBusqueda.Siguiente;
+                if (NodoBusqueda.Siguiente.Dato == Dato)
+                {
+                    return NodoBusqueda;
+                }
+            }
+            return null;
+        }
+        public void BorrarNodo(string Dato)
+        {
+            if (!EstaVacia())
+            {
+                nodoActual = Buscar(Dato);
+
+                if (nodoActual == null)
+                {
+                    return;
+                }
+
+                Nodo nodoAnterior = BuscarAnterior(Dato);
+                nodoAnterior.Siguiente = nodoActual.Siguiente;
+                nodoActual.Siguiente = null;
+            }
+        }
+        public Nodo BuscarIndice(int Indice)
+        {
+            int indice = -1;
+
+            if (EstaVacia())
+            {
+                return null;
+            }
+
+            nodoActual = primerNodo;
+
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                indice++;
+
+                if (indice == Indice)
+                {
+                    return nodoActual;
+                }
+            }
+            return null;
+        }
     }
 }
--- ED_LSL/Program.cs
+++ ED_LSL/Program.cs
@@ -19,26 +19,56 @@ namespace ED_LSL
             lista.AgregarNodo("Cinco");
 
             Console.WriteLine(lista.RecorrerLista());
+            Console.WriteLine("<==Agregando nodo al inicio de la lista==>\n");
+            lista.AgregarNodoInicio("Cero");
+            Console.WriteLine(lista.RecorrerLista());
 
-            if (lista.EstaVacia())
+            Console.WriteLine("\n<==Busqueda de nodo en la lista==>\n");
+            Nodo nodo = lista.Buscar("Cuatro");
+            if (nodo == null)
             {
-                Console.WriteLine("La lista esta vacia");
+                Console.WriteLine("Dato no encontrado");
             }
             else
             {
-                Console.WriteLine("La lista tiene datos");
+                Console.WriteLine($"Se encontro el nodo con valor {nodo.Dato}");
             }
 
-            lista.VaciarLista();
+            Console.WriteLine("\n<==Eliminando nodo de la lista==>\n");
+            lista.BorrarNodo("Cuatro");
+            Console.WriteLine(lista.RecorrerLista());
 
-            if (lista.EstaVacia())
+            Console.WriteLine("\n<==Busqueda por indice en la lista==>\n");
+            Nodo nodoIndice = lista.BuscarIndice(3);
+            int Indice = 3;
+            if (nodoIndice == null)
             {
-                Console.WriteLine("La lista esta vacia");
+                Console.WriteLine("Indice no encontrado");
             }
             else
             {
-                Console.WriteLine("La lista tiene datos");
+                Console.WriteLine($"Se encontro el indice {Indice} con valor {nodoIndice.Dato}");
             }
+
+            //if (lista.EstaVacia())
+            //{
+            //    Console.WriteLine("La lista esta vacia");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("La lista tiene datos");
+            //}
+
+            //lista.VaciarLista();
+
+            //if (lista.EstaVacia())
+            //{
+            //    Console.WriteLine("La lista esta vacia");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("La lista tiene datos");
+            //}
 
             Console.ReadKey();
             
